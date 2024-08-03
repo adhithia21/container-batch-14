f8f78564d1e7c275d46a19ac4fc00b5723b44852
adhithia21 <116649263+adhithia21@users.noreply.github.com>
2024-08-03 10:55:14 +0700

    Update main.go

--- main.go
+++ main.go
@@ -13,7 +13,8 @@ func main() {
 			"code":     200,
 			"language": "go version go1.20.6",
 			"message":  "Server running on port 3000",
-			"version":  "v2.3",
+			"version":  "v3.0",
+			"description": "belajar fluxcd image update automation",
 		})
 	})
 	r.Run("0.0.0.0:3000")
